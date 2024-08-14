fbd686cc85706d2a8d77936e8b8a2cc35f0ef743
WillianCavalcante <cavalcante1968@hotmail.com>
2024-08-13 22:26:08 -0300

    13082024will

---
+++ Willian_Cavalcante/20240806_admin-template/includes/menu.php
@@ -0,0 +1,14 @@
+<nav class="main-menu">
+				<div class="main-menu-inside">
+					<div class="title">Dashboard</div>
+					<ul>
+						<li>
+							<a href="?page=inicial">
+								<i class="fa-solid fa-gauge"></i>
+								Inicial
+							</a>
+						</li>
+						<li><a href="?page=form"><i class="fa-solid fa-gauge"></i>Formulario</a></li>
+					</ul>
+				</div>
+			</nav>
