4e77e53a2114b0b38ff739313263b877a8da5efa
gui2745123 <cabralguilherme2745@gmail.com>
2024-08-13 22:23:42 -0300

    13/08/2024
    aula do do dia 13 PHP

---
+++ Guilherme Cabral/20240806_admin-template/index.php
@@ -0,0 +1,53 @@
+<?php
+include_once('includes/connect.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+		rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/vendors/fontawesome-free-6.6.0-web/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+
+	<div class="main-container">
+		<?php
+		include_once('includes/header.php')
+		?>
+		<div class="mc-bottom">
+			<?php
+			include_once('includes/menu.php')
+			?>
+			<section class="main-content">
+				<div class="main-content-inside">
+					<?php
+					$page_defaul = 'inicial';
+					$page = $page_defaul;
+					if (!empty($_GET['page'])); {
+						$page = $_GET['page'];
+					}
+					$page_url = 'pages/' . $page . '.php';
+					if (file_exists($page_url)) {
+						include_once($page_url);
+					}else{
+						include_once('pages/' . $page_default . '.php');
+					}
+					include_once($page_url)
+					?>
+				</div>
+			</section>
+		</div>
+	</div>
+
+</body>
+
+</html>
