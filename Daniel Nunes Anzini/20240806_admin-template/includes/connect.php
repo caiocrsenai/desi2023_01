88e0ab71c8dfac4c6baeea9440c9b326df220493
danielanzini <daniel.anzinii@gmail.com>
2024-08-13 22:31:47 -0300

    20240801

---
+++ Daniel Nunes Anzini/20240806_admin-template/includes/connect.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect("localhost", "root","", "desi202301_admin")
+or die (mysql_error('erro na conexão'));
+
+?>
---
+++ Daniel Nunes Anzini/20240806_admin-template/includes/menu.php
@@ -0,0 +1,20 @@
+<nav class="main-menu">
+				<div class="main-menu-inside">
+					<div class="title">Dashboard</div>
+					<ul>
+						<li>
+							<a href="?page=inicial">
+								<i class="fa-solid fa-gauge"></i>
+								Inicial
+							</a>
+						</li>
+						<li>
+							<a href="?page=form">
+								<i class="fa-solid fa-gauge"></i>
+								Formulário
+							</a>
+						</li>
+
+					</ul>
+				</div>
+			</nav>
