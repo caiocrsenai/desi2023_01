66d80cf95d1a4a77852f78eecce362b42ede5fd9
nicolaskoslo <nicolas_koslowski@estudante.sesisenai.org.br>
2024-08-15 22:27:32 -0300

    15/08/24
    .

--- nicolas_koslowski/20240806_admin-template/includes/menu.php
+++ nicolas_koslowski/20240806_admin-template/includes/menu.php
@@ -1,20 +1,26 @@
 <nav class="main-menu">
-				<div class="main-menu-inside">
-					<div class="title">Dashboard</div>
-					<ul>
-						<li>
-							<a href="?page=inicial">
-								<i class="fa-solid fa-gauge"></i>
-								Inicial
-							</a>
-						</li>
-						<ul>
-						<li>
-							<a href="page=form">
-								<i class="fa-solid fa-gauge"></i>
-								formulário
-							</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
+    <div class="main-menu-inside">
+        <div class="title">Dashboard</div>
+        <ul>
+            <li>
+                <a href="?page=inicial">
+                    <i class="fa-solid fa-gauge"></i>
+                    Inicial
+                </a>
+            </li>
+            <li>
+                <a href="?page=form">
+                    <i class="fa-solid fa-gauge"></i>
+                    Formulário
+                </a>
+            </li>
+            <li>
+                <a href="?page=usuarios">
+                    <i class="fa-solid fa-gauge"></i>
+                    usuarios
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
