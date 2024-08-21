4373f693ccaa8d77a738b33585d9552ed221722d
gui2745123 <cabralguilherme2745@gmail.com>
2024-08-20 22:00:59 -0300

    aula 20 do oito

--- Guilherme Cabral/20240806_admin-template/includes/header.php
+++ Guilherme Cabral/20240806_admin-template/includes/header.php
@@ -1,35 +1,35 @@
 <header class="mc-top">
-	<div class="header-logo">
-		<img src="assets/images/logo-white.png">
-	</div>
-	<div class="header-content">
-		<div class="hc-search">
-			<label class="search-container">
-				<i class="fa-solid fa-magnifying-glass"></i>
-				<input type="text">
-			</label>
-		</div>
-		<div class="hc-options">
-			<div class="option-item">
-				<i class="fa-regular fa-bell"></i>
-			</div>
-			<div class="option-item">
-				<i class="fa-solid fa-globe"></i>
-			</div>
-			<div class="option-item">
-				<i class="fa-solid fa-gear"></i>
-			</div>
-			<div class="option-item menu">
-				<div class="menu-container">
-					<div class="image"
-						style="background-image: url(https://pbs.twimg.com/media/FTIuycrUYBIq6KM.jpg);">
-					</div>
-					<div class="infos-ct">
-						<div class="name">Guilherme Cabral</div>
-						<div class="info">Desenvolvedor Full-Stack</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="header-logo">
+        <img src="assets/images/logo-white.png">
+    </div>
+    <div class="header-content">
+        <div class="hc-search">
+            <label class="search-container">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="text">
+            </label>
+        </div>
+        <div class="hc-options">
+            <div class="option-item">
+                <i class="fa-regular fa-bell"></i>
+            </div>
+            <div class="option-item">
+                <i class="fa-solid fa-globe"></i>
+            </div>
+            <div class="option-item">
+                <i class="fa-solid fa-gear"></i>
+            </div>
+            <div class="option-item menu">
+                <div class="menu-container">
+                    <div class="image"
+                        style="background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn7KAhD4HLNtS525KxMnGiUW36_a_YRN1s0ow6_PS8idxUvvMmG3pXTZkD4AvQkstXhyQ&usqp=CAU);">
+                    </div>
+                    <div class="infos-ct">
+                        <div class="name">Guilherme Cabral</div>
+                        <div class="info">Desenvolvedor Full-Stack</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
--- Guilherme Cabral/20240806_admin-template/index.php
+++ Guilherme Cabral/20240806_admin-template/index.php
@@ -2,7 +2,7 @@
 include_once('includes/connect.php');
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
 	<meta charset="UTF-8">
@@ -15,24 +15,21 @@ include_once('includes/connect.php');
 		rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="assets/vendors/fontawesome-free-6.6.0-web/css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	
+	<script src="assets/js/jquery-3.7.1.min.js"></script>
 </head>
-
 <body>
 
 	<div class="main-container">
-		<?php
-		include_once('includes/header.php')
-		?>
+		<?php include_once('includes/header.php'); ?>
 		<div class="mc-bottom">
-			<?php
-			include_once('includes/menu.php')
-			?>
+			<?php include_once('includes/menu.php'); ?>
 			<section class="main-content">
 				<div class="main-content-inside">
 					<?php
-					$page_defaul = 'inicial';
-					$page = $page_defaul;
-					if (!empty($_GET['page'])); {
+					$page_default = 'inicial';
+					$page = $page_default;
+					if (!empty($_GET['page'])) {
 						$page = $_GET['page'];
 					}
 					$page_url = 'pages/' . $page . '.php';
@@ -41,7 +38,6 @@ include_once('includes/connect.php');
 					}else{
 						include_once('pages/' . $page_default . '.php');
 					}
-					include_once($page_url)
 					?>
 				</div>
 			</section>
