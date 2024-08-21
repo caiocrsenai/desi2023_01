4baf9b021905df5b94ee315485312764d21c7c59
Gabriel Bruno <gabrielbruno7891@gmail.com>
2024-08-20 21:59:01 -0300

    v20240820_validation

--- Gabriel Aguiar Bruno/20240806_admin-template/pages/form.php
+++ Gabriel Aguiar Bruno/20240806_admin-template/pages/form.php
@@ -24,7 +24,6 @@ if (!empty($_POST)) {
     }
 }
 ?>
-v1.0.0
 <div class="container-box cb-form-max-width align-center flex-1">
     <div class="cb-header">
         <div class="cb-title">Formulário</div>
@@ -66,14 +65,39 @@ v1.0.0
                 <input type="text" name="cep" required>
             </label>
 
+
             <label>
                 <div class="lbl">Estado</div>
-                <input type="text" name="id_state">
+                <select name="id_state">
+                    <option selected disabled style="display: none;">Selecione o estado</option>
+                    <?php
+                    $sql = "SELECT * FROM state";
+                    $result = $con->query($sql);
+
+                    if ($result->num_rows > 0){
+                        while ($row = $result->fetch_object()){
+                            echo '<option value="'.$row->id_state.'">'. $row->nome .' ('.$row->uf.')</option>';
+                        }
+                    }
+                    ?>
+                </select>
             </label>
 
             <label>
                 <div class="lbl">Cidade</div>
-                <input type="text" name="id_city">
+                <select name="id_city">
+                    <option selected disabled style="display: none;">Selecione a cidade</option>
+                    <?php
+                    $sql = "SELECT * FROM city";
+                    $result = $con->query($sql);
+
+                    if ($result->num_rows > 0){
+                        while ($row = $result->fetch_object()){
+                            echo '<option value="'.$row->id_city.'"data-uf="'. $row->uf .'" class="hide">'. $row->nome .'</option>';
+                        }
+                    }
+                    ?>
+                </select>
             </label>
 
             <div class="form-actions">
@@ -85,6 +109,11 @@ v1.0.0
 </div>
 
 <script>
+
+    $(function(){
+        alert('Vou sair mais cedo!!!');
+    });
+
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     function validateForm() {
