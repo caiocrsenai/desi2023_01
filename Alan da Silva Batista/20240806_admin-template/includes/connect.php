96d4b4b92be02e13acb81db17607509b42426795
Alan da S Batista <alanbatista556688@gmail.com>
2024-08-20 21:59:35 -0300

    v20240820_2200

--- Alan da Silva Batista/20240806_admin-template/includes/connect.php
+++ Alan da Silva Batista/20240806_admin-template/includes/connect.php
@@ -1,5 +1,3 @@
-<?php 
-   $con=mysqli_connect("localhost" , "root" , "", "desi202301_admin")
-   or die (mysql_error('Erro na conexão'));
- 
-?>   
+<?php
+    $con=mysqli_connect("localhost", "root", "","desi202301_admin");
+?>
--- Alan da Silva Batista/20240806_admin-template/index.php
+++ Alan da Silva Batista/20240806_admin-template/index.php
@@ -1,8 +1,6 @@
 <?php
-
 include_once('includes/connect.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,35 +15,31 @@ include_once('includes/connect.php');
 		rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="assets/vendors/fontawesome-free-6.6.0-web/css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<script src="assets/js/jquery-3.7.1.min.js"></script>
+	
 </head>
 
 <body>
 
 	<div class="main-container">
-		<?php
-		include_once('includes/header.php')
-		?>
-		<div class="mc-bottom"> 
-			<?php
-			include_once('includes/menu.php');
-			?>
+		<?php include_once('includes/header.php'); ?>
+		<div class="mc-bottom">
+			<?php include_once('includes/menu.php'); ?>
 			<section class="main-content">
 				<div class="main-content-inside">
 					<?php
-					$page_defaut ='inicial';
-					$page = $page_defaut;
+					$page_default = 'inicial';
+					$page = $page_default;
 					if (!empty($_GET['page'])) {
 						$page = $_GET['page'];
-						
 					}
-                    $page_url = 'pages/' . $page . '.php';
-					if(file_exists($page_url)){
+					$page_url = 'pages/' . $page . '.php';
+					if (file_exists($page_url)) {
 						include_once($page_url);
 					}else{
-					
-						include_once('pages/' .$page_defaut . '.php');
+						include_once('pages/' . $page_default . '.php');
 					}
-					
 					?>
 				</div>
 			</section>
