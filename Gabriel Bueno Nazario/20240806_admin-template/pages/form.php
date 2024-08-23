ce5f8caa0855f5a8032d55ec49e56580c4f41443
Gabriel Bueno Nazario <gabrielbuenonazario@gmail.com>
2024-08-22 22:23:30 -0300

    22/08/2023
    Tá entregue

--- Gabriel Bueno Nazario/20240806_admin-template/pages/form.php
+++ Gabriel Bueno Nazario/20240806_admin-template/pages/form.php
@@ -68,7 +68,7 @@ if (!empty($_POST)) {
             <label>
                 <div class="lbl">Estado</div>
                 <select name="id_state">
-                    <option selected disabled style="display: none;">Selecione o estado</option>
+                    <option selected disabled style="display: none;" value="">Selecione o estado</option>
                     <?php
                     $sql = "SELECT * FROM state";
                     $result = $con->query($sql);
@@ -85,7 +85,7 @@ if (!empty($_POST)) {
             <label>
                 <div class="lbl">Cidade</div>
                 <select name="id_city">
-                    <option selected disabled style="display: none;">Selecione o cidade</option>
+                    <option selected disabled style="display: none;" value="">Selecione o cidade</option>
                     <?php
                     $sql = "SELECT * FROM city";
                     $result = $con->query($sql);
@@ -108,11 +108,32 @@ if (!empty($_POST)) {
 </div>
 
 <script>
-    $(function() {
-        alert('ae');
+    _qs('#userForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const _this = this,
+        _element = _this._qsa('input, select');
+
+        _element.forEach(function(_element) {
+            const val = _element.value;
+
+            if (val == '') {
+                _element.classList.add('error');
+            } else {
+                _element.classList.remove('error');
+            }
+        });
     });
 
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    _qs('#userForm')._qsa('input, select').forEach(function(_element) {
+        _element.addEventListener('keyup', function(event) {
+            const _this = this,
+            val = _element.value;
+
+            _this.classList.remove('error');
+        });
+    });
+
+    /*const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     function validateForm() {
         const form = document.getElementById('userForm');
@@ -133,5 +154,5 @@ if (!empty($_POST)) {
             event.preventDefault();
             alert('Favor preencher todos os campos obrigatórios.');
         }
-    });
+    });*/
 </script>
