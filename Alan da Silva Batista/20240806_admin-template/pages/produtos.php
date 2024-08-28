c5db3385918b31db3c708a0a72ce407cf97143d5
Alan da S Batista <alanbatista556688@gmail.com>
2024-08-27 22:20:44 -0300

    v20240827_2220

---
+++ Alan da Silva Batista/20240806_admin-template/pages/produtos.php
@@ -0,0 +1,56 @@
+<?php
+
+if (!empty($_POST)) {
+    $sql = "
+    INSERT INTO product (nome, idcategoria, codebar, preco)
+    VALUES
+    (
+    
+    '" . $_POST ['name'] . "',
+    '" . $_POST ['categoria'] . "',
+    '" . $_POST ['codebar'] . "',
+    '" . $_POST ['preco'] . "',
+
+)";
+
+   $result = $con->query($sql);
+}    
+
+?>
+
+
+
+<div class="container-box cb-form-max-width align-center flex-1">
+    <div class="cb-header">
+        <div class="cb-title">Produtos</div>
+    </div>
+    <div class="cb-body">
+        <form method="POST" action="" id="productForm" name="producForm" novalidate>
+            <label>
+                <div class="lbl">Nome</div>
+                <input type="text" name="name" required>
+            </label>
+
+            <label>
+                <div class="lbl">Categoria</div>
+                <input type="text" name="categoria" required>
+            </label>
+
+            <label>
+                <div class="lbl">Código de Barras (EAN-13)</div>
+                <input type="text" name="codebar" maxlength="13">
+            </label>
+
+            <label>
+                <div class="lbl">Preco</div>
+                <input type="number" min="0.00"  max="10000.00" step="0.10" name="preco" />
+            </label>
+
+        
+                <div class="form-actions">
+                <button type ="submit" >Enviar</button>
+
+                </div>
+        </form>
+    </div>
+    </div>
