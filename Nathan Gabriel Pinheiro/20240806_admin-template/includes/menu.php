ed1aef1ab4817b7604b24260b7724019fd628c21
Nathan Gabriel Pinheiro <nathangpinheiro@gmail.com>
2024-08-27 22:20:34 -0300

    v20240827_2220

--- Nathan Gabriel Pinheiro/20240806_admin-template/includes/menu.php
+++ Nathan Gabriel Pinheiro/20240806_admin-template/includes/menu.php
@@ -4,20 +4,26 @@
         <ul>
             <li>
                 <a href="?page=inicial">
-                    <i class="fa-solid fa-gauge"></i>
+                    <i class="fa-solid fa-house"></i>
                     Inicial
                 </a>
             </li>
             <li>
                 <a href="?page=form">
-                    <i class="fa-solid fa-gauge"></i>
+                    <i class="fa-regular fa-rectangle-list"></i>
                     Formulário
                 </a>
             </li>
             <li>
                 <a href="?page=usuarios">
-                    <i class="fa-solid fa-gauge"></i>
+                    <i class="fa-solid fa-user-plus"></i>
                     Usuários
+                </a>
+            </li>
+            <li>
+                <a href="?page=produtos">
+                    <i class="fa-solid fa-file-import"></i>
+                    Produtos
                 </a>
             </li>
         </ul>
