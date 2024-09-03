154ae9a03c825c59ef977c22373e5046b68bae9f
danielanzini <daniel.anzinii@gmail.com>
2024-09-03 19:03:09 -0300

    dani02-09-24

---
+++ Daniel Nunes Anzini/20240806_admin-template/includes/menu.php
@@ -0,0 +1,31 @@
+<nav class="main-menu">
+    <div class="main-menu-inside">
+        <div class="title">Dashboard</div>
+        <ul>
+            <li>
+                <a href="?page=inicial">
+                    <i class="fa-brands fa-playstation"></i>
+                    Inicial
+                </a>
+            </li>
+            <li>
+                <a href="?page=usuario">
+                    <i class="fa-solid fa-gauge"></i>
+                    Usuário
+                </a>
+            </li>
+            <li>
+                <a href="?page=usuarios">
+                    <i class="fa-solid fa-user-plus"></i>
+                    Usuários
+                </a>
+            </li>
+            <li>
+                <a href="?page=produtos">
+                    <i class="fa-solid fa-file-import"></i>
+                    Produtos 
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
