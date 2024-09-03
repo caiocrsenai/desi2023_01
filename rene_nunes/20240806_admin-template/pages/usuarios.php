4271756e9200d98eba3221912ece0cb6bfee5d3b
renedevilla <renedevillanunes@gmail.com>
2024-09-02 22:04:11 -0300

    att02092024-add-delete

--- rene_nunes/20240806_admin-template/pages/usuarios.php
+++ rene_nunes/20240806_admin-template/pages/usuarios.php
@@ -1,3 +1,18 @@
+<?php
+if (!empty($_GET['id'])) {
+    $idUser = $_GET['id'];
+
+    $sql = 'DELETE FROM user WHERE user.id = '.$idUser.';';
+
+    $result = $con->query($sql);
+    if ($con ->affected_rows > 0) {
+        echo "<script>alert('Usuário excluido com sucesso!')</script>";
+    }
+}
+?>
+
+
+
 <div class="container-box flex-1">
     <div class="cb-header">
         <div class="cb-title">Vendas</div>
@@ -32,7 +47,7 @@
                                     </a>
                                 </td>
                                 <td>
-                                    <div class="btn-status color-red">
+                                    <div class="delete-user btn-status color-red" data-id="<?php echo $row->id; ?> " data-username="<?php echo $row->username; ?>">
                                         <i class="fa-regular fa-trash-can"></i>
                                     </div>
                                 </td>
@@ -46,3 +61,20 @@
         </div>
     </div>
 </div>
+
+<script>
+    _qsa('.delete-user').forEach(function(_element) {
+
+        _element.addEventListener('click', function(e) {
+            const _this = this;
+            dataId = _this.getAttribute('data-id');
+            dataUsername = _this.getAttribute('data-username');
+
+            if (confirm('Você deseja realmente excluir este usuário?')) {
+                alert('Excluir usuário: ' + dataUsername);
+                window.location.href = '?page=usuarios&id=' + dataId;
+            }
+        });
+
+    });
+</script>
