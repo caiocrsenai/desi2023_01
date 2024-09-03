4b72cbc7c689e154ed18628ff99240dfaf9c5b57
Nathan Gabriel Pinheiro <nathangpinheiro@gmail.com>
2024-09-02 22:02:39 -0300

    v20240209_2202

--- Nathan Gabriel Pinheiro/20240806_admin-template/pages/usuario.php
+++ Nathan Gabriel Pinheiro/20240806_admin-template/pages/usuario.php
@@ -10,14 +10,14 @@ if (!empty($_POST)) {
     if ($idUser) {
         $sql = "
         UPDATE user SET 
-        pass = '322', 
-        username = 'rolaa', 
-        email = 'rolaa@mailinator.com', 
-        name = 'Roludoo', 
-        cep = '322', 
-        id_city = '5', 
-        id_state = '12' 
-        WHERE user.id = 65
+        pass = '" . $_POST['pass'] . "', 
+        username = '" . $_POST['username'] . "', 
+        email = '" . $_POST['email'] . "', 
+        name = '" . $_POST['name'] . "', 
+        cep = '" . $_POST['cep'] . "', 
+        id_city = '" . $_POST['id_city'] . "', 
+        id_state = '" . $_POST['id_state'] . "' 
+        WHERE user.id = " . $idUser . "
         ";
     } else {
         $sql = "
@@ -41,7 +41,12 @@ if (!empty($_POST)) {
     $result = $con->query($sql);
 
     if ($result) {
-        echo "<script>alert('Usuário " . $_POST['username'] . " cadastrado com sucesso!')</script>";
+        $action = "cadastrado";
+        if ($idUser) {
+            $action = "alterado";
+        }
+
+        echo "<script>alert('Usuário " . $_POST['username'] . " " . $action . " com sucesso!')</script>";
     }
 }
 
