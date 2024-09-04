279f6b7e9411ed01e87838cc1174e92182b823be
João Victor Corrêa <joaogamer0807@gmail.com>
2024-09-03 22:10:56 -0300

    03092024

--- Joao Victor Correa/20240806_admin-template/pages/usuarios.php
+++ Joao Victor Correa/20240806_admin-template/pages/usuarios.php
@@ -1,3 +1,16 @@
+<?php
+if (!empty($_GET['id'])) {
+    $idUser = $_GET['id'];
+
+    $sql = "DELETE FROM user WHERE user.id = " . $idUser . ";";
+
+    $result = $con->query($sql);
+    if ($con->affected_rows > 0) {
+        echo "<script>alert('Usuário excluido com sucesso!')</script>";
+    }
+}
+?>
+
 <div class="container-box flex-1">
     <div class="cb-header">
         <div class="cb-title">Vendas</div>
@@ -9,11 +22,10 @@
                     <tr>
                         <th>Nome</th>
                         <th>Email</th>
-                        <th>Usuário </th>
+                        <th>Usuario</th>
                         <th width="10px">Alterar</th>
                         <th width="10px">Excluir</th>
                     </tr>
-
                 </thead>
                 <tbody>
                     <?php
@@ -22,7 +34,7 @@
 
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_object()) {
-                            ?>
+                    ?>
                             <tr>
                                 <td><?php echo $row->name; ?></td>
                                 <td><?php echo $row->email; ?></td>
@@ -33,12 +45,12 @@
                                     </a>
                                 </td>
                                 <td>
-                                    <div class="btn-status color-red">
+                                    <div class="delete-user btn-status color-red" data-id="<?php echo $row->id; ?>">
                                         <i class="fa-regular fa-trash-can"></i>
                                     </div>
                                 </td>
                             </tr>
-                            <?php
+                    <?php
                         }
                     }
                     ?>
@@ -47,3 +59,17 @@
         </div>
     </div>
 </div>
+
+<script>
+    _qsa('.delete-user').forEach(function(_element) {
+        _element.addEventListener('click', function(e) {
+            const _this = this,
+                dataId = _this.getAttribute('data-id');
+
+            if (confirm('Você deseja realmente excluir o usuário?')) {
+                //alert('Excluir usuario: ' + dataId);
+                window.location.href = '?page=usuarios&id=' + dataId;
+            }
+        });
+    });
+</script>
