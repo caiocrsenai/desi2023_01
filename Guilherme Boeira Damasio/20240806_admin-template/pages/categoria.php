eea8912b5345b0cf38297b02cd9e8f53fba79f66
Guilherme-b-damasio <153876817+Guilherme-b-damasio@users.noreply.github.com>
2024-09-05 20:28:11 -0300

    feat: Adicionando crud de categoria

---
+++ Guilherme Boeira Damasio/20240806_admin-template/pages/categoria.php
@@ -0,0 +1,72 @@
+<?php
+$idCategory = false;
+$categoryInfos = false;
+
+if (!empty($_GET['id'])) {
+    $idCategory = $_GET['id'];
+}
+
+if (!empty($_POST)) {
+
+    if ($idCategory) {
+        $sql = "UPDATE category SET 
+        name = '" . $_POST['name'] . "', 
+        description = '" . $_POST['description'] . "'  
+        WHERE id = '" . $idCategory . "'
+        ";
+    } else {
+        $sql = "INSERT INTO category 
+        (name, description) 
+        VALUES 
+        (
+            '" . $_POST['name'] . "', 
+            '" . $_POST['description'] . "'
+        )
+        ";
+    }
+
+    $result = $con->query($sql);
+
+    if ($result) {
+        $action = "cadastrado";
+        if($idCategory){
+            $action = "alterado";
+        }
+
+        echo "<script>alert('Categoria " . $_POST['name'] . " " . $action . " com sucesso!')</script>";
+    }
+}
+
+if ($idCategory) {
+    $sql = "SELECT * FROM category WHERE id = " . $idCategory;
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        $categoryInfos = $result->fetch_object();
+    }
+}
+
+?>
+
+<div class="container-box cb-form-max-width align-center flex-1">
+    <div class="cb-header">
+        <div class="cb-title">Categoria</div>
+    </div>
+    <div class="cb-body">
+        <form method="POST" action="" id="categoryForm" name="categoryForm" novalidate>
+            <label>
+                <div class="lbl">Nome</div>
+                <input type="text" name="name" value="<?php echo $categoryInfos ? $categoryInfos->name : '' ?>" required>
+            </label>
+
+            <label>
+                <div class="lbl">Descrição</div>
+                <textarea maxlength="100" type="text" name="description" value="<?php echo $categoryInfos ? $categoryInfos->description : '' ?>" required></textarea>
+            </label>
+
+            <div class="form-actions">
+                <button type="submit">Enviar</button>
+            </div>
+        </form>
+    </div>
+</div>
--- Guilherme Boeira Damasio/20240806_admin-template/pages/categorias.php
+++ Guilherme Boeira Damasio/20240806_admin-template/pages/categorias.php
@@ -1,19 +1,19 @@
 <?php
 if (!empty($_GET['id'])) {
-    $idProduct = $_GET['id'];
+    $idCategory = $_GET['id'];
 
-    $sql = "DELETE FROM product WHERE product.id = " . $idProduct . ";";
+    $sql = "DELETE FROM category WHERE category.id = " . $idCategory . ";";
 
     $result = $con->query($sql);
     if ($con->affected_rows > 0) {
-        echo "<script>alert('Produto excluido com sucesso!')</script>";
+        echo "<script>alert('Categoria excluida com sucesso!')</script>";
     }
 }
 ?>
 
 <div class="container-box flex-1">
     <div class="cb-header">
-        <div class="cb-title">Produtos</div>
+        <div class="cb-title">Categorias</div>
     </div>
     <div class="cb-body">
         <div class="table-container">
@@ -21,15 +21,14 @@ if (!empty($_GET['id'])) {
                 <thead>
                     <tr>
                         <th>Nome</th>
-                        <th>Categoria</th>
-                        <th>Preço</th>
+                        <th>Descrição</th>
                         <th width="10px">Alterar</th>
                         <th width="10px">Excluir</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    $sql = "SELECT product.name, product.id, product.price, product.codebar, product.id_category, category.name as category_name FROM product inner join category on category.id = product.id_category";
+                    $sql = "SELECT * FROM category";
                     $result = $con->query($sql);
 
                     if ($result->num_rows > 0) {
@@ -37,15 +36,14 @@ if (!empty($_GET['id'])) {
                     ?>
                             <tr>
                                 <td><?php echo $row->name; ?></td>
-                                <td><?php echo $row->category_name; ?></td>
-                                <td><?php echo $row->price; ?></td>
+                                <td><?php echo $row->description; ?></td>
                                 <td>
-                                    <a href="?page=produto&id=<?php echo $row->id; ?>" class="btn-status color-blue">
+                                    <a href="?page=categoria&id=<?php echo $row->id; ?>" class="btn-status color-blue">
                                         <i class="fa-regular fa-pen-to-square"></i>
                                     </a>
                                 </td>
                                 <td>
-                                    <div class="delete-product btn-status color-red" data-id="<?php echo $row->id; ?>">
+                                    <div class="delete-category btn-status color-red" data-id="<?php echo $row->id; ?>">
                                         <i class="fa-regular fa-trash-can"></i>
                                     </div>
                                 </td>
@@ -61,14 +59,14 @@ if (!empty($_GET['id'])) {
 </div>
 
 <script>
-    _qsa('.delete-product').forEach(function(_element) {
+    _qsa('.delete-category').forEach(function(_element) {
         _element.addEventListener('click', function(e) {
             const _this = this,
                 dataId = _this.getAttribute('data-id');
 
             if (confirm('Você deseja realmente excluir o produto?')) {
                 //alert('Excluir produto: ' + dataId);
-                window.location.href = '?page=produtos&id=' + dataId;
+                window.location.href = '?page=categorias&id=' + dataId;
             }
         });
     });
