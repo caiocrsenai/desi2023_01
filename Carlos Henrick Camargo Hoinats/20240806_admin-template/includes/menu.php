5dbffcfb897cefcddba47c5a3d7190cfc4c5f013
carlos-henrick <141372667+carlos-henrick@users.noreply.github.com>
2024-09-05 21:58:09 -0300

    20240905_Categoria

--- Carlos Henrick Camargo Hoinats/20240806_admin-template/includes/menu.php
+++ Carlos Henrick Camargo Hoinats/20240806_admin-template/includes/menu.php
@@ -32,6 +32,18 @@
                     Produtos 
                 </a>
             </li>
+            <li>
+                <a href="?page=categoria">
+                    <i class="fa-solid fa-file-import"></i>
+                    Categoria 
+                </a>
+            </li>
+            <li>
+                <a href="?page=categorias">
+                    <i class="fa-solid fa-file-import"></i>
+                    Categorias 
+                </a>
+            </li>
         </ul>
     </div>
 </nav>
