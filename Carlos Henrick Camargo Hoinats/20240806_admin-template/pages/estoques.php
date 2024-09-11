75f71a84d9af48b6745b8e56bc9731de36f02fd7
carlos-henrick <141372667+carlos-henrick@users.noreply.github.com>
2024-09-10 22:28:41 -0300

    v20240810_estoque

--- Carlos Henrick Camargo Hoinats/20240806_admin-template/pages/estoques.php
+++ Carlos Henrick Camargo Hoinats/20240806_admin-template/pages/estoques.php
@@ -1,37 +1,39 @@
 <?php
 if (!empty($_GET['id'])) {
-    $idProduct = $_GET['id'];
+    $idStock = $_GET['id'];
 
-    $sql = "DELETE FROM product WHERE product.id = " . $idProduct . ";";
+    $sql= "DELETE FROM stock WHERE stock.id = ".$idStock .";";
 
     $result = $con->query($sql);
-    if ($con->affected_rows > 0) {
-        echo "<script>alert('Produto excluido com sucesso!')</script>";
+    if ($con -> affected_rows > 0) {
+        echo "<script>alert('Produto excluído com sucesso!')</script>";
     }
 }
-?>
 
+?>
 <div class="container-box flex-1">
     <div class="cb-header">
-        <div class="cb-title">Produtos</div>
+        <div class="cb-title">Estoques</div>
     </div>
     <div class="cb-body">
         <div class="table-container">
             <table>
                 <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>Categoria</th>
-                        <th>Preço</th>
+                        <th>Produto</th>
+                        <th>Quantidade</th>
                         <th width="10px">Alterar</th>
                         <th width="10px">Excluir</th>
                     </tr>
                 </thead>
                 <tbody>
+
                     <?php
-                    $sql = "SELECT pro.name, pro.id, pro.price, cat.name AS cat_name
-                            FROM product AS pro
-                            JOIN category AS cat ON cat.id = pro.id_category";
+                    $sql = "SELECT stk.id, stk.quantity, pdt.name
+                    from stock as stk
+                    inner JOIN product as pdt
+                    on pdt.id = stk.id_product
+                    ";
                     $result = $con->query($sql);
 
                     if ($result->num_rows > 0) {
@@ -39,20 +41,24 @@ if (!empty($_GET['id'])) {
                     ?>
                             <tr>
                                 <td><?php echo $row->name; ?></td>
-                                <td><?php echo $row->cat_name; ?></td>
-                                <td><?php echo $row->price; ?></td>
+                                <td><?php ?><?php echo $row->quantity; ?></td>
                                 <td>
-                                    <a href="?page=produto&id=<?php echo $row->id; ?>" class="btn-status color-blue">
+                                    <a href="?page=estoque&id=<?php echo $row->id; ?>" class="btn-status color-blue ">
+
                                         <i class="fa-regular fa-pen-to-square"></i>
                                     </a>
                                 </td>
+
                                 <td>
-                                    <div class="delete-product btn-status color-red" data-id="<?php echo $row->id; ?>">
+                                    <div class="delete-stock btn-status color-red " data-id="<?php echo $row->id; ?>">
                                         <i class="fa-regular fa-trash-can"></i>
+
                                     </div>
                                 </td>
                             </tr>
+
                     <?php
+
                         }
                     }
                     ?>
@@ -63,15 +69,17 @@ if (!empty($_GET['id'])) {
 </div>
 
 <script>
-    _qsa('.delete-product').forEach(function(_element) {
+    _qsa('.delete-stock').forEach(function(_element){
         _element.addEventListener('click', function(e) {
             const _this = this,
                 dataId = _this.getAttribute('data-id');
 
-            if (confirm('Você deseja realmente excluir o produto?')) {
+            if (confirm('Você deseja realmente excluir o estoque?')){
                 //alert('Excluir usuario: ' + dataId);
-                window.location.href = '?page=produtos&id=' + dataId;
+                window.location.href = '?page=estoques&id=' + dataId;
             }
         });
     });
+    
+
 </script>
--- Carlos Henrick Camargo Hoinats/20240806_admin-template/pages/produto.php
+++ Carlos Henrick Camargo Hoinats/20240806_admin-template/pages/produto.php
@@ -1,5 +1,4 @@
 <?php
-
 $idProduct = false;
 $productInfos = false;
 
@@ -9,24 +8,24 @@ if (!empty($_GET['id'])) {
 
 if (!empty($_POST)) {
 
-    if($idProduct){
-      $sql="UPDATE product SET 
-      name = '" . $_POST['name'] . "', 
-      id_category = '" . $_POST['id_category'] . "',
-      codebar = '" . $_POST['codebar'] . "',
-      price = '" . $_POST['price'] . "'        
-       WHERE product.id = ". $idProduct . " 
-      ";
-    }else{
+    if ($idProduct) {
+        $sql = "UPDATE product SET 
+        name = '" . $_POST['name'] . "', 
+        id_category = '" . $_POST['id_category'] . "', 
+        codebar = '" . $_POST['codebar'] . "', 
+        price = '" . $_POST['price'] . "' 
+        WHERE product.id = ". $idProduct ."
+        ";
+    } else {
         $sql = "INSERT INTO product 
         (name, id_category, codebar, price) 
         VALUES 
-        ( 
+        (
             '" . $_POST['name'] . "',
             '" . $_POST['id_category'] . "',
             '" . $_POST['codebar'] . "',
             '" . $_POST['price'] . "'
-            )
+        )
         ";
     }
 
@@ -34,25 +33,25 @@ if (!empty($_POST)) {
 
     if ($result) {
         $action = "cadastrado";
-        if($idProduct){
+        if ($idProduct) {
             $action = "alterado";
         }
 
-        echo "<script>alert('Produto " . $_POST['name'] . " ". $action ." com sucesso!')</script>";
+        echo "<script>alert('Produto " . $_POST['name'] . " " . $action . " com sucesso!')</script>";
     }
 }
 
 if ($idProduct) {
-    $sql = "SELECT * FROM product WHERE id= " . $idProduct;
+    $sql = "SELECT * FROM product WHERE id = " . $idProduct;
     $result = $con->query($sql);
-
     if ($result->num_rows > 0) {
         $productInfos = $result->fetch_object();
     }
 }
 
-?>
 
+
+?>
 <div class="container-box cb-form-max-width align-center flex-1">
     <div class="cb-header">
         <div class="cb-title">Produtos</div>
@@ -61,22 +60,32 @@ if ($idProduct) {
         <form method="POST" action="" id="productForm" name="productForm" novalidate>
             <label>
                 <div class="lbl">Nome</div>
-                <input type="text" name="name" value="<?php echo $productInfos ?  $productInfos->name : '' ?>" required>
+                <input type="text" name="name" value="<?php echo $productInfos ? $productInfos->name : '' ?>" required>
             </label>
 
             <label>
                 <div class="lbl">Categoria</div>
-                <input type="text" name="id_category" value="<?php echo $productInfos ?  $productInfos->id_category : '' ?>" required>
+                <select name="id_category" required>
+                    <option value=''>Selecione</option>
+                    <?php
+                        $sql = "SELECT id, name FROM category";
+                        $categorias = $con->query($sql);
+                        while ($row = $categorias->fetch_object()) {
+                            $selected = ($productInfos ? $productInfos->id_category : '') == $row->id ? 'selected' : '';
+                            echo '<option '.  $selected .' value="' . $row->id . '">' . $row->name . '</option>';
+                        }
+                    ?>
+                </select>
             </label>
 
             <label>
                 <div class="lbl">Código de Barras (EAN-13)</div>
-                <input type="text" name="codebar" value="<?php echo $productInfos ?  $productInfos->codebar : '' ?>" maxlength="13">
+                <input type="text" name="codebar" value="<?php echo $productInfos ? $productInfos->codebar : '' ?>" maxlength="13">
             </label>
 
             <label>
                 <div class="lbl">Preco</div>
-                <input type="number" min="0.00" max="10000.00" step="0.10" name="price" value="<?php echo $productInfos ?  $productInfos->price : '' ?>" />
+                <input type="number" min="0.00" max="10000.00" step="0.10" name="price" value="<?php echo $productInfos ? $productInfos->price : '' ?>" />
             </label>
 
             <div class="form-actions">
