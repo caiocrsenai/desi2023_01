29c40f6bba8e57999670df4802c0537eac1e6fe4
Gabriel Bruno <gabrielbruno7891@gmail.com>
2024-09-10 22:11:45 -0300

    v20240911 - adicionado estoque e estoques

--- Gabriel Aguiar Bruno/20240806_admin-template/includes/menu.php
+++ Gabriel Aguiar Bruno/20240806_admin-template/includes/menu.php
@@ -4,44 +4,56 @@
         <ul>
             <li>
                 <a href="?page=inicial">
-                    <i class="fa-brands fa-playstation"></i>
+                    <i class="fa-solid fa-house"></i></i>
                     Inicial
                 </a>
             </li>
             <li>
                 <a href="?page=usuario">
-                    <i class="fa-solid fa-gauge"></i>
+                    <i class="fa-solid fa-user-plus"></i>
                     Usuário
                 </a>
             </li>
             <li>
                 <a href="?page=usuarios">
-                    <i class="fa-solid fa-user-plus"></i>
+                    <i class="fa-solid fa-users"></i>
                     Usuários
                 </a>
             </li>
             <li>
                 <a href="?page=produto">
-                    <i class="fa-solid fa-file-import"></i>
+                    <i class="fa-solid fa-square"></i>
                     Produto 
                 </a>
             </li>
             <li>
                 <a href="?page=produtos">
-                    <i class="fa-solid fa-file-import"></i>
+                    <i class="fa-solid fa-clone"></i>
                     Produtos 
                 </a>
             </li>
             <li>
                 <a href="?page=categoria">
-                    <i class="fa-solid fa-file-import"></i>
+                    <i class="fa-solid fa-file"></i>
                     Categoria 
                 </a>
             </li>
             <li>
                 <a href="?page=categorias">
-                    <i class="fa-solid fa-file-import"></i>
+                    <i class="fa-solid fa-copy"></i>
                     Categorias 
+                </a>
+            </li>
+            <li>
+                <a href="?page=estoque">
+                    <i class="fa-solid fa-cube"></i>
+                    Estoque 
+                </a>
+            </li>
+            <li>
+                <a href="?page=estoques">
+                    <i class="fa-solid fa-cubes"></i>
+                    Estoques 
                 </a>
             </li>
         </ul>
--- Gabriel Aguiar Bruno/20240806_admin-template/pages/estoques.php
+++ Gabriel Aguiar Bruno/20240806_admin-template/pages/estoques.php
@@ -1,54 +1,52 @@
 <?php
 if (!empty($_GET['id'])) {
-    $idProduct = $_GET['id'];
+    $idStock = $_GET['id'];
 
-    $sql = "DELETE FROM product WHERE product.id = " . $idProduct . ";";
+    $sql = "DELETE FROM stock WHERE stock.id = " . $idStock . ";";
 
     $result = $con->query($sql);
     if ($con->affected_rows > 0) {
-        echo "<script>alert('Produto excluido com sucesso!')</script>";
+        echo "<script>alert('Estoque excluido com sucesso!')</script>";
     }
 }
 ?>
 
 <div class="container-box flex-1">
     <div class="cb-header">
-        <div class="cb-title">Produtos</div>
+        <div class="cb-title">Estoques</div>
     </div>
     <div class="cb-body">
         <div class="table-container">
             <table>
                 <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>Categoria</th>
-                        <th>Preço</th>
+                        <th>Produto</th>
+                        <th>Quantidade</th>
                         <th width="10px">Alterar</th>
                         <th width="10px">Excluir</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    $sql = "SELECT product.id_category,product.id,product.name,product.codebar,product.price,category.name AS category_name 
-                            FROM `product` 
-                            INNER JOIN category 
-                            ON category.id = product.id_category;";
+                    $sql = "SELECT stock.id_product,stock.id,stock.quantity,product.name AS product_name 
+                            FROM `stock` 
+                            INNER JOIN product 
+                            ON product.id = stock.id_product;";
                     $result = $con->query($sql);
 
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_object()) {
                     ?>
                             <tr>
-                                <td><?php echo $row->name; ?></td>
-                                <td><?php echo $row->category_name; ?></td>
-                                <td><?php echo $row->price; ?></td>
+                                <td><?php echo $row->product_name; ?></td>
+                                <td><?php echo $row->quantity; ?></td>
                                 <td>
-                                    <a href="?page=produto&id=<?php echo $row->id; ?>" class="btn-status color-blue">
+                                    <a href="?page=estoque&id=<?php echo $row->id; ?>" class="btn-status color-blue">
                                         <i class="fa-regular fa-pen-to-square"></i>
                                     </a>
                                 </td>
                                 <td>
-                                    <div class="delete-product btn-status color-red" data-id="<?php echo $row->id; ?>">
+                                    <div class="delete-stock btn-status color-red" data-id="<?php echo $row->id; ?>">
                                         <i class="fa-regular fa-trash-can"></i>
                                     </div>
                                 </td>
@@ -64,14 +62,14 @@ if (!empty($_GET['id'])) {
 </div>
 
 <script>
-    _qsa('.delete-product').forEach(function(_element) {
+    _qsa('.delete-stock').forEach(function(_element) {
         _element.addEventListener('click', function(e) {
             const _this = this,
                 dataId = _this.getAttribute('data-id');
 
-            if (confirm('Você deseja realmente excluir o produto?')) {
-                //alert('Excluir produto: ' + dataId);
-                window.location.href = '?page=produtos&id=' + dataId;
+            if (confirm('Você deseja realmente excluir o estoque?')) {
+                //alert('Excluir categoria: ' + dataId);
+                window.location.href = '?page=estoques&id=' + dataId;
             }
         });
     });
