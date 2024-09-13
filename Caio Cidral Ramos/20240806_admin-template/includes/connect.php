437b2458f8dc2f4ea76a1bcaf9c4b54e9bca24d7
GabrielBuenoNazario <128106481+GabrielBuenoNazario@users.noreply.github.com>
2024-09-12 23:37:34 -0300

    to fazendo aqui os negocios

--- Caio Cidral Ramos/20240806_admin-template/includes/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $con=mysqli_connect("localhost", "root", "","desi202301_admin");
-?>
--- Felippe Kleimmann/20240806_admin-template/includes/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$con=mysqli_connect ("localhost", "root", "", "bd_php")
-or die (mysql_error('Erro na conexao'));
-
-?>
