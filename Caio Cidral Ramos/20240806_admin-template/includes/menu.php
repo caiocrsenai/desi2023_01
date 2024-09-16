049f0a64f4c56d1128a6f7d77c420b33608f6395
marciosenhorinha <caio.ramos@edu.sc.senai.br>
2024-09-16 18:49:03 -0300

    Alterações Menu e Estoque

--- Caio Cidral Ramos/20240806_admin-template/includes/menu.php
+++ Caio Cidral Ramos/20240806_admin-template/includes/menu.php
@@ -44,6 +44,18 @@
                     Categorias
                 </a>
             </li>
+            <li>
+                <a href="?page=estoque">
+                    <i class="fa-solid fa-file-import"></i>
+                    Estoque
+                </a>
+            </li>
+            <li>
+                <a href="?page=estoques">
+                    <i class="fa-solid fa-file-import"></i>
+                    Estoques
+                </a>
+            </li>
         </ul>
     </div>
 </nav>
--- Caio Cidral Ramos/20240806_admin-template/pages/estoques.php
+++ Caio Cidral Ramos/20240806_admin-template/pages/estoques.php
@@ -1,38 +1,38 @@
 <?php
 if (!empty($_GET['id'])) {
-    $idProduct = $_GET['id'];
+    $idStock = $_GET['id'];
 
-    $sql = "DELETE FROM product WHERE product.id = " . $idProduct . ";";
+    $sql = "DELETE FROM stock WHERE stock.id = " . $idStock . ";";
 
     $result = $con->query($sql);
     if ($con->affected_rows > 0) {
-        echo "<script>alert('Produto excluido com sucesso!')</script>";
+        echo "<script>alert('Estoque excluido com sucesso!')</script>";
     }
 }
 ?>
 
 <div class="container-box flex-1">
     <div class="cb-header">
-        <div class="cb-title">Produtos</div>
+        <div class="cb-title">Estoque</div>
     </div>
     <div class="cb-body">
         <div class="table-container">
             <table>
                 <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>Categoria</th>
-                        <th>Preço</th>
+                        <th>Produto</th>
+                        <th>Quantidade</th>
                         <th width="10px">Alterar</th>
                         <th width="10px">Excluir</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    $sql = "SELECT pro.name, pro.id, pro.price, cat.name AS cat_name
-                            FROM product AS pro
-                            JOIN category AS cat
-                            ON cat.id = pro.id_category";
+                    $sql = "SELECT stk.quantity, pdt.name
+                    FROM stock as stk
+                    INNER JOIN product as pdt
+                    on pdt.id = stk.id_product
+                    ";
                     $result = $con->query($sql);
 
                     if ($result->num_rows > 0) {
@@ -40,10 +40,9 @@ if (!empty($_GET['id'])) {
                     ?>
                             <tr>
                                 <td><?php echo $row->name; ?></td>
-                                <td><?php echo $row->cat_name; ?></td>
-                                <td><?php echo $row->price; ?></td>
+                                <td><?php echo $row->quantity; ?></td>
                                 <td>
-                                    <a href="?page=produto&id=<?php echo $row->id; ?>" class="btn-status color-blue">
+                                    <a href="?page=estoque&id=<?php echo $row->id; ?>" class="btn-status color-blue">
                                         <i class="fa-regular fa-pen-to-square"></i>
                                     </a>
                                 </td>
@@ -71,7 +70,7 @@ if (!empty($_GET['id'])) {
 
             if (confirm('Você deseja realmente excluir o produto?')) {
                 //alert('Excluir usuario: ' + dataId);
-                window.location.href = '?page=produtos&id=' + dataId;
+                window.location.href = '?page=estoques&id=' + dataId;
             }
         });
     });
