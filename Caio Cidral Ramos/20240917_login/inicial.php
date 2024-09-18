ef6c90e6e5700bb70e15e255381b0f27d171df25
marciosenhorinha <caio.ramos@edu.sc.senai.br>
2024-09-17 22:26:24 -0300

    v20240917_2226
    SA Templates e Login

---
+++ Caio Cidral Ramos/20240917_login/inicial.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+ob_start();
+
+function isUserLoggedIn()
+{
+    // Verifica se o usuário está logado e se o session_id() é válido
+    if (isset($_SESSION['session_id']) && $_SESSION['session_id'] === session_id()) {
+        return true; // Usuário está logado
+    } else {
+        return false; // Usuário não está logado
+    }
+}
+
+echo '<pre>';
+var_dump($_SESSION);
+var_dump(isUserLoggedIn());
+echo '</pre>';
+?>
+
+<a href="login.php">Login</a>
---
+++ Caio Cidral Ramos/20240917_sa_templates/themes/pages/inicial.php
@@ -0,0 +1,38 @@
+<div class="page page-initial">
+    <div class="main-filters">
+        Filtros
+    </div>
+    <div class="properties-container">
+
+        <h3>
+            Imóveis para comprar em Joinville<br>
+            <span>1072 Resultados</span>
+        </h3>
+
+        <div class="propertie">
+            <div class="image"></div>
+            <div class="infos-container">
+                <div class="type">Apartamento padrão</div>
+                <div class="neighborhood">Guanabara - Joinville</div>
+                <div class="price">R$ 250.000,00</div>
+                <div class="icons-info">
+                    <ul>
+                        <li>
+                            <i class="fa-solid fa-bed"></i>
+                            2 quartos
+                        </li>
+                        <li>
+                            <i class="fa-regular fa-square"></i>
+                            36m²
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-car"></i>
+                            1 vaga
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
